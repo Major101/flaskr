5d77c278f1c8411c5fd16f1a8388382a052e9a2a
Major101 <majorMail101@gmail.com>
2020-03-25 19:44:35 +0500

    Configuring setup and adding delete function for posts

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -42,6 +42,7 @@ def create():
             )
             db.commit()
             return redirect(url_for('blog.index'))
+    return render_template('blog/create.html')
 
 
 def get_post(id, check_author=True):
@@ -61,7 +62,7 @@ def get_post(id, check_author=True):
     return post
 
 
-@bp.route('/<int:id>/update', method=['GET', 'POST'])
+@bp.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
 def update(id):
     post = get_post(id)
@@ -87,3 +88,13 @@ def update(id):
             return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
+
+
+@bp.route('/<int:id>/delete', methods=['POST,'])
+@login_required
+def delete(id):
+    get_post(id)
+    db = get_db()
+    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
